39bb5628623fcfd9afb0b19df57b25c6a94803bf
Cristian Torres <cris.torres15@gmail.com>
2019-04-11 14:07:03 -0600

    [CHANGE] Show address and alias in autocomplete for societe

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1086,7 +1086,7 @@ class Form
 		}
 
 		// On recherche les societes
-		$sql = "SELECT s.rowid, s.nom as name, s.name_alias, s.client, s.fournisseur, s.code_client, s.code_fournisseur";
+		$sql = "SELECT s.rowid, s.nom as name, s.name_alias, s.address, s.client, s.fournisseur, s.code_client, s.code_fournisseur";
 		$sql.= " FROM ".MAIN_DB_PREFIX ."societe as s";
 		if (!$user->rights->societe->client->voir && !$user->socid) $sql .= ", ".MAIN_DB_PREFIX."societe_commerciaux as sc";
 		$sql.= " WHERE s.entity IN (".getEntity('societe').")";
@@ -1165,9 +1165,13 @@ class Form
 						$label=$obj->name;
 					}
 
-					if(!empty($obj->name_alias)) {
-						$label.=' ('.$obj->name_alias.')';
-					}
+                    if(!empty($obj->name_alias)) {
+                        $label.=' (@ '.$obj->name_alias.')';
+                    }
+
+                    if(!empty($obj->address)) {
+                        $label.=' (# '.$obj->address.')';
+                    }
 
 					if ($showtype)
 					{
